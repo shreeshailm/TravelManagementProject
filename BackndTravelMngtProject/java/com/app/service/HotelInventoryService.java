83cf9d3827f5038bfa68287f5ac553cf121cd069
shreeshailm <138684441+shreeshailm@users.noreply.github.com>
2023-08-19 01:13:53 +0530

    Delete BackndTravelMngtProject directory

--- BackndTravelMngtProject/java/com/app/service/HotelInventoryService.java
+++
@@ -1,8 +0,0 @@
-package com.app.service;
-
-import java.time.LocalDate;
-
-public interface HotelInventoryService {
-	String addInventory(LocalDate Date,Integer availableRoom,Integer hotelId);
-	String deleteInventory(Long id);
-}
